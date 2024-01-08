e8289459d2eabffe0c4eba9e6a6a045f688501c3
StrangePlant05 <iamzeth05@gmail.com>
2024-01-08 23:11:15 +0800

    new level designer

---
+++ LevelDesigner/main.js
@@ -0,0 +1,203 @@
+let levelDisplay = document.getElementById('levelDisplay');
+
+let tileXInput = document.getElementById('tileX')
+let tileYInput = document.getElementById('tileY')
+let tileSizeInput = document.getElementById('tileSize')
+let refreshButton = document.getElementById('refresh')
+let hideButton = document.getElementById('hide')
+let submitButton = document.getElementById('submit')
+let lockButton = document.getElementById('lock')
+
+let brushSelect = document.getElementsByName('brushSelect')
+
+let typeInput = document.getElementById('tileType');
+let idInput = document.getElementById('tileId');
+let spanRowInput = document.getElementById('spanRow');
+let spanColumnInput = document.getElementById('spanColumn');
+
+let selectedTile;
+
+let uiToggle = true;
+let lock = false;
+
+let tiles = [];
+let displayTiles = [];
+
+let tileX;
+let tileY;
+let tileSize;
+
+let colors = [
+    "#00000000",
+    "black",
+    "orange",
+    "brown",
+    "red",
+    "green"
+]
+
+refreshButton.addEventListener("click", () => {
+    refreshDisplay();
+});
+lockButton.addEventListener("click", () => {
+    refreshDisplay();
+    tileXInput.disabled = true;
+    tileYInput.disabled = true;
+    tileSizeInput.disabled = true;
+
+    lock = true;
+});
+
+hideButton.addEventListener("click", () => {
+    uiToggle = !uiToggle;
+    let display = uiToggle ? "flex" : "none";
+    let width = uiToggle ? "300px" : "fit-content"
+    document.getElementById("levelProperties").style.display = display;
+    document.getElementById("tileProperties").style.display = display;
+    document.getElementById("brush").style.display = display;
+    document.getElementById("level").style.width = width;
+    refreshButton.style.display = display;
+    submitButton.style.display = display;
+});
+
+function refreshDisplay() {
+    if (lock) {
+        tiles.forEach(e => {
+            e.tile.style.background = colors[e.type]
+        });
+    } else {
+        tiles = [];
+        levelDisplay.innerHTML = '';
+
+        tileX = parseInt(tileXInput.value);
+        tileY = parseInt(tileYInput.value);
+        tileSize = parseInt(tileSizeInput.value);
+    
+        if (Number.isInteger(tileX) && Number.isInteger(tileY) && Number.isInteger(tileSize)) {
+            for (let y = 0; y < tileY; y++) {
+                let row = document.createElement("div");
+                for (let x = 0; x < tileX; x++) {
+                    let tile = document.createElement("div");
+                    tile.style.width = tileSize + "px";
+                    tile.style.height = tileSize + "px";
+                    tile.style.borderWidth = "1px";
+                    tile.style.border = "solid";
+                    tile.style.borderColor = "#b2b2b2";
+                    tile.style.userSelect = "none"
+                    tile.style.display = "grid"
+                    tile.style.placeItems = "center"
+                    tile.addEventListener("click", () => {
+                        tileClick(tile);
+                    })
+                    row.appendChild(tile);
+
+                    tiles.push({
+                        id: x + ":" + y  + ":" + 0,
+                        x,
+                        y,
+                        tileSize,
+                        spanRow: 1,
+                        spanColumn: 1,
+                        type: 0,
+                        tile
+                    })
+                }
+                levelDisplay.appendChild(row);
+            }
+        }
+    }
+}
+
+function tileClick(tile) {
+    let currentTile;
+    tiles.forEach(e => {
+        e.tile.innerHTML = '';
+        if (e.tile == tile) {
+            currentTile = e;
+            let type;
+            for (let i = 0; i < brushSelect.length; i++) {
+                type = 0;
+                if (brushSelect[i].checked) {
+                    type = i;
+                    break;
+                }
+            }
+            type -= 1;
+            selectedTile = currentTile;
+            currentTile.tile.innerHTML = 'o';
+            typeInput.value = currentTile.type;
+            idInput.value = currentTile.id;
+            spanRowInput.value = currentTile.spanRow;
+            spanColumnInput.value = currentTile.spanColumn;
+
+            if (type > colors.length - 1 || type == -1) return;   
+            currentTile.type = type;
+            currentTile.id = currentTile.x + ":" + currentTile.y + ":" + type;
+            refreshDisplay();
+            return;
+        }
+    });
+}
+
+spanColumnInput.addEventListener("input", () => {
+    let newSpan = parseInt(spanColumnInput.value)
+    if (selectedTile) {
+        if (newSpan >= 1 && (newSpan + selectedTile.x) < tileX) {
+            for (let j = 0; j < selectedTile.spanRow; j++) {
+                for (let i = selectedTile.x; i < selectedTile.spanColumn + selectedTile.x; i++) {
+                    let oldSpan = tiles.filter((tile) => { return tile.x == i && tile.y == selectedTile.y + j});
+                    oldSpan.forEach(tile => {
+                        if (!(tile == selectedTile) && tile.id == selectedTile.id) {
+                            tile.spanColumn = 1;
+                            tile.type = 0;
+                            tile.id = tile.x + ":" + tile.y + ":" + 0;
+                        }
+                    });
+                }
+                for (let i = selectedTile.x; i < newSpan + selectedTile.x; i++) {
+                    let span = tiles.filter((tile) => { return tile.x == i && tile.y == selectedTile.y + j});
+                    span.forEach(tile => {
+                        if (tile.type == 0) {
+                            tile.id = selectedTile.id;
+                            tile.type = selectedTile.type;
+                        }
+                    });
+                }
+            }
+            selectedTile.spanColumn = newSpan;
+        }
+        refreshDisplay();
+    }
+});
+
+spanRowInput.addEventListener("input", () => {
+    let newSpan = parseInt(spanRowInput.value)
+    if (selectedTile) {
+        if (newSpan >= 1 && (newSpan + selectedTile.y) < tileY) {
+            for (let j = 0; j < selectedTile.spanColumn; j++) {
+                for (let i = selectedTile.y; i < selectedTile.spanRow + selectedTile.y; i++) {
+                    let oldSpan = tiles.filter((tile) => { return tile.y == i && tile.x == selectedTile.x + j});
+                    console.log(i)
+                    oldSpan.forEach(tile => {
+                        if (!(tile == selectedTile) && tile.id == selectedTile.id) {
+                            tile.spanRow = 1;
+                            tile.type = 0;
+                            tile.id = tile.x + ":" + tile.y + ":" + 0;
+                        }
+                    });
+                }
+                for (let i = selectedTile.y; i < newSpan + selectedTile.y; i++) {
+                    let span = tiles.filter((tile) => { return tile.y == i && tile.x == selectedTile.x + j});
+                    span.forEach(tile => {
+                        if (tile.type == 0) {
+                            tile.id = selectedTile.id;
+                            tile.type = selectedTile.type;
+                        }
+                    });
+                }
+            }
+            selectedTile.spanRow = newSpan;
+        }
+        refreshDisplay();
+    }
+});
--- scripts/Stage.js
+++ scripts/Stage.js
@@ -1,64 +1,27 @@
 class Stage {
-    constructor({ walls, players, entities, triggers, spawnPoint}) {
-        this.walls = walls;
-        this.players = players
-        this.entities = entities;
-        this.triggers = triggers    
-        this.spawnPoint = spawnPoint
-
-        wallsLayout = [
-             
-        ]
+    constructor({ layout, cellSize }) {
+        this.wallsLayout = layout;
+        this.cellSize = cellSize;
+        this.walls = [];
+        this.entities = [];
+        this.buttons = [];
     }
 
-    createWalls(wallsLayout) {
-        
+    initStage() {
+        for (let y = 0; y < this.wallsLayout.length; y++) {
+            for (let x = 0; y < this.wallsLayout[y].length; x++) {
+                switch (this.wallsLayout[y][x]) {
+                    case 1: 
+                        this.walls.push(new Sprite({x: x * this.cellSize, y: y * this.cellSize, width: this.cellSize, height: this.cellSize, color: "#000"}));
+                        break;
+                    case 2: 
+                        this.buttons.push(new TriggerBox({x: x * this.cellSize, y: y * this.cellSize + (this.cellSize / 2), width: this.cellSize, height: (this.cellSize / 2), color: "orange"}))
+                        break;
+                }
+            }
+        }
     }
-
-
     update(context) {
-        context.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-        let playerXAverage = (player1.position.x + player2.position.x) / 2;
-        let playerYAverage = (player1.position.y + player2.position.y) / 2
-        let cameraOffsetX = (playerXAverage - window.innerWidth /2) * 0.1;
-        let cameraOffsetY = (playerYAverage - window.innerHeight /2) * 0.1;
-        for(let i = 0; i < Utils.walls.length; i++) {
-            Utils.walls[i].position.x -= cameraOffsetX;
-            Utils.walls[i].position.y -= cameraOffsetY;
-            Utils.walls[i].draw(context);
-        }
-        // Camera Head
-        player1.position.x -= cameraOffsetX;
-        player1.position.y -= cameraOffsetY;
-        
-        player2.position.x -= cameraOffsetX;
-        player2.position.y -= cameraOffsetY;
-        
-        object.position.x -= cameraOffsetX;
-        object.position.y -= cameraOffsetY;
-
-        spawnPoint.x -= cameraOffsetX;
-        spawnPoint.y -= cameraOffsetY;
-
-
-        if (Utils.getDistance(player1.position, player2.position) > 3000) {
-            player2.position = { ...spawnPoint }
-            player2.velocityX = 0;
-            player2.velocityY = 0;
-            player2.dx = 0;
-
-            player1.position = { ...spawnPoint }
-            player1.velocityX = 0;
-            player1.velocityY = 0;
-            player1.dx = 0;
-        }
-        player1.update(context);
-        player2.update(context)
-        object.update(context);
-        
-        
-        context.fillStyle = "green"
-        context.fillRect(spawnPoint.x, spawnPoint.y, 10, 10)
     }
 }
